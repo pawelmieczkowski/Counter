c698529d6cc9c5fe87ea310cffd7b19cd4fca6e0
Salata <pawemie@gmail.com>
2022-02-01 00:51:11 +0100

    fixed setting active css class on selected tab. Was broken because of transition to hashrouter

--- frontend/src/components/MenuBar.tsx
+++ frontend/src/components/MenuBar.tsx
@@ -1,20 +1,22 @@
 import {Nav, Container, Row, Navbar} from "react-bootstrap";
 import './MenuBar.css'
+import React from "react";
+import {useLocation} from 'react-router-dom';
 
 
 export const MenuBar = () => {
-
+    const location = useLocation();
     return (
         <Container className={"h-100 ps-0"} fluid>
             <Row>
                 <Navbar variant={"dark"} className={"ps-0"}>
                     <Nav className="flex-column w-100 h-100 lh-lg">
-                        <Nav.Link href="#/" active={window.location.pathname === "/"} className={"p-0 m-0"}>
+                        <Nav.Link href="#/" active={location.pathname === "/"} className={"p-0 m-0"}>
                             <Container fluid className={"py-2"}>
                                 Main Page
                             </Container>
                         </Nav.Link>
-                        <Nav.Link href="#/counter-page" active={window.location.pathname === "/counter-page"}
+                        <Nav.Link href="#/counter-page" active={location.pathname === "/counter-page"}
                                   className={"p-0 m-0"}>
                             <Container fluid className={"py-2"}>
                                 Counter Page
